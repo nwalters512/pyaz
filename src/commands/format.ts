b28aeaa493ff13257133a6b71c884264359452ca
Nathan Walters <nwalters512@gmail.com>
2020-11-21 15:58:09 -0800

    Scaffold linting and formatting

---
+++ src/commands/format.ts
@@ -0,0 +1,7 @@
+import { Command, flags } from "@oclif/command";
+
+export default class Format extends Command {
+  static description = "Fix problems with code";
+
+  async run() {}
+}
---
+++ src/commands/lint.ts
@@ -0,0 +1,7 @@
+import { Command, flags } from "@oclif/command";
+
+export default class Lint extends Command {
+  static description = "Check code for formatting errors and best practices";
+
+  async run() {}
+}
