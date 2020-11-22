5524972e1225a663cbc09308a44cc70cda598401
Nathan Walters <nwalters512@gmail.com>
2020-11-21 17:02:36 -0800

    Log formatting

--- src/commands/format.ts
+++ src/commands/format.ts
@@ -1,10 +1,13 @@
 import { Command, flags } from '@oclif/command';
+import chalk from 'chalk';
 import { runPrettier } from '../lib/prettier';
 
 export default class Format extends Command {
   static description = 'Fix problems with code';
 
   async run() {
+    console.log(chalk.cyan('Formatting'));
     await runPrettier({ fix: true });
+    console.log(chalk.cyan('Formatting completed'));
   }
 }
