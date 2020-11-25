3b8410ea438cd343630333a654e59433e5f2bb16
Nathan Walters <nwalters512@gmail.com>
2020-11-25 15:39:11 -0800

    Add ESLint linting

--- src/commands/lint.ts
+++ src/commands/lint.ts
@@ -1,6 +1,7 @@
 import { Command, flags } from '@oclif/command';
 import chalk from 'chalk';
 import { runPrettier } from '../lib/prettier';
+import { runEslint } from '../lib/eslint';
 
 export default class Lint extends Command {
   static description = 'Check code for formatting errors and best practices';
@@ -14,6 +15,7 @@ export default class Lint extends Command {
     const { argv } = this.parse(Lint);
     console.log(chalk.blue('Linting...'));
     await runPrettier({ paths: argv });
+    await runEslint({ paths: argv });
     console.log(chalk.blue('Linting complete'));
   }
 }
--- src/lib/eslint.ts
+++ src/lib/eslint.ts
@@ -1,1 +1,21 @@
-import { CLIEngine } from 'eslint';
+import { CLIEngine, ESLint } from 'eslint';
+
+const DEFAULT_PATHS = ['.'];
+
+export const runEslint = async ({ fix = false, paths: pathsOption = [] }) => {
+  const paths: string[] =
+    pathsOption && pathsOption.length ? pathsOption : [DEFAULT_PATHS];
+
+  const eslint = new ESLint({ fix });
+
+  const results = await eslint.lintFiles(paths);
+
+  if (fix) {
+    await ESLint.outputFixes(results);
+  }
+
+  const formatter = await eslint.loadFormatter('stylish');
+  const resultText = formatter.format(results);
+
+  console.log(resultText);
+};
