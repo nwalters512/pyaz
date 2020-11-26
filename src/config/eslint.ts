74e6b7f4a615327d4659317b3b46aca648aeedc2
Nathan Walters <nwalters512@gmail.com>
2020-11-25 16:02:32 -0800

    Update ESLint config

--- src/config/eslint.ts
+++ src/config/eslint.ts
@@ -1,3 +1,31 @@
-export const makeEslintConfig = () => ({
-  extends: ['eslint:recommended', 'prettier'],
+import type { Linter } from 'eslint';
+
+const OFF = 'off';
+
+export const makeEslintConfig = (): Linter.Config => ({
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': OFF,
+  },
 });
